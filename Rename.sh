c5c51ea4271fc07bd3c1226cf22ea70f5999b236
thisisamirv <thisisamirv@gmail.com>
2022-01-04 17:08:42 +0330

    BIDS reformat

--- Rename.sh
+++ Rename.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cd /Users/av/Desktop/rs-fMRI/Data
+cd /Users/av/Desktop/rs-fMRI/Caltech/Nifti
 
 for i in *; do
-    mv /Users/av/Desktop/rs-fMRI/Data/$i/session_1/anat_1/mprage.nii.gz /Users/av/Desktop/rs-fMRI/Data/$i/session_1/anat_1/$i.nii.gz
+    sub_name="sub-"
+    sub_name="$sub_name$i"
+    file_name="sub-"
+    file_name="$file_name$i"
+    file_name_suffix="_ses-1_task-rest_acq-TR2500_bold"
+    file_name="$file_name$file_name_suffix"
+    mkdir -p /Users/av/Desktop/rs-fMRI/Caltech/Nifti/$sub_name/ses-1/func
+    mv /Users/av/Desktop/rs-fMRI/Caltech/Nifti/$i/session_1/rest_1/rest.nii.gz /Users/av/Desktop/rs-fMRI/Caltech/Nifti/$sub_name/ses-1/func/$file_name.nii.gz
 done
