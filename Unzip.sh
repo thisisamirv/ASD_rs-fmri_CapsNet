53266d90580163f8c363587cf7425b8bf5c62300
thisisamirv <thisisamirv@gmail.com>
2022-01-09 15:50:18 +0330

    removed extra stuff

--- Unzip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /Users/av/Desktop/rs-fMRI/Caltech/Nifti
-
-for i in *; do
-    gunzip /Users/av/Desktop/rs-fMRI/Caltech/Nifti/$i/ses-1/func/*.nii.gz
-    gunzip /Users/av/Desktop/rs-fMRI/Caltech/Nifti/$i/ses-1/anat/*.nii.gz
-done
